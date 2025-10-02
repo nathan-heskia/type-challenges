ceecd539c51d221ac7043fc00d36d5ea7c069887
Nathan Heskia <nathan.heskia@gmail.com>
2025-10-02 10:29:02 -0700

    04 - easy pick

--- questions/00004-easy-pick/template.ts
+++ questions/00004-easy-pick/template.ts
@@ -1,1 +1,12 @@
-type MyPick<T, K> = any
+// Explanation:
+//
+// T: The source object type you're picking from
+// K extends keyof T: The keys you want to pick
+// keyof T gets all possible keys from type T
+// extends keyof T constrains K to only be valid keys of T
+// This prevents picking non-existent properties!
+type MyPick<T, K extends keyof T> = {
+  // [P in K]: Iterates over each key P in the union K
+  // T[P]: Gets the type of property P from the original type T
+  [P in K]: T[P]
+} // Creates a new object with only the selected properties
